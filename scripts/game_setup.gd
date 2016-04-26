51f9424136ea28fdd892ee92639006e5c3b0b153
Laura Berger-Uichanco <laura-h-95@live.com.au>
2016-04-26 16:55:35 +1000

    Code for exiting the game

--- scripts/game_setup.gd
+++ scripts/game_setup.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 var grid
 var is_setup = false
-
+var curr_speed = 1.0
+var exiting = false
+var exit_timer
 var c0
 var c1
 
@@ -12,11 +14,22 @@ var grid_template = preload("res://scenes/grid.scn")
 func _ready():
 	set_process_input(true)
 	setup("res://levels/level0.txt")
+	exit_timer = get_node("exit_timer")
 
 func _input(event):
-	if(!is_setup || event.is_echo() || !event.is_pressed()):
+	if(!is_setup || event.is_echo()):
 		return
 	
+	if(event.is_action("exit")):
+		if(event.is_pressed()):
+			exiting = true
+			exit_timer.start()
+		else:
+			exiting = false
+			exit_timer.stop()
+	
+	if(!event.is_pressed()):
+		return
 	if (event.is_action("up")):
 		grid.up()
 	elif (event.is_action("down")):
@@ -43,4 +56,11 @@ func _on_display_timer_timeout():
 	if(!is_setup):
 		return
 	var colour = grid.next()
+	#TODO: make the screen that colour
 	print(colour)
+
+
+func _on_exit_timer_timeout():
+	if(exiting):
+		#TODO: quit to menu
+		print("exit")
