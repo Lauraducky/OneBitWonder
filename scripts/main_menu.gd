e8a77e0770c06dbeee2e71c04bbd7d3e8557d3d5
Laura Berger-Uichanco <laura-h-95@live.com.au>
2016-04-28 10:33:00 +1000

    Hooked up methods

---
+++ scripts/main_menu.gd
@@ -0,0 +1,38 @@
+
+extends VBoxContainer
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+
+
+
+func _on_play_pressed():
+	print("play")
+	pass # replace with function body
+
+
+func _on_level_select_pressed():
+	print("level")
+	pass # replace with function body
+
+
+func _on_settings_pressed():
+	print("settings")
+	pass # replace with function body
+
+
+func _on_tutorial_pressed():
+	print("tutorial")
+	pass # replace with function body
+
+
+func _on_quit_pressed():
+	print("quit")
+	pass # replace with function body
